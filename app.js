32929f3c937ffc363f8d9c0387d8bbfdb58fd501
Adam Amatavivadhana <adam.amata@yahoo.com>
2022-08-09 11:58:24 +0200

    added partials

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require("express");
+const hbs = require("hbs");
 const app = express();
 const port = 3000; 
 
 app.set("views", __dirname + "/views"); //setting views directory to /views
 app.set("view engine", "hbs"); //setting view engine to hbs 
-app.use(express.static('public')); //all static files in public folder are accessible by express
+app.use(express.static('public')); //all static files in public folder are accessible by express //defining path of public folder 
+
+hbs.registerPartials(__dirname + "/views/partials"); //partials = reusable html content 
 
 const users = ['user1', 'user2', 'user3', 'user4'];
 
-app.get('/', (req, res) => {
+//setting routes depending on endpoints "/", "/contact" 
+app.get('/', (req, res) => { 
     res.render("index", {heading: 'Welcome to my homepage!', users: users}); 
 });
 
---
+++ views/partials/footer.hbs
@@ -0,0 +1,3 @@
+<div>
+    this is a footer
+</div>
