e31cf3cb1c27b38c64c806c1484efe90a2989a57
Adam Amatavivadhana <adam.amata@yahoo.com>
2022-08-09 10:59:23 +0200

    added layout.hbs

--- app.js
+++ app.js
@@ -5,8 +5,14 @@ const port = 3000;
 app.set("views", __dirname + "/views"); 
 app.set("view engine", "hbs"); 
 
+const users = ['Adam', 'Sofia', 'Jom', 'Brendan'];
+
 app.get('/', (req, res) => {
-    res.render("index", {heading: 'Welcome to my homepage!'}); 
+    res.render("index", {heading: 'Welcome to my homepage!', users: users}); 
+});
+
+app.get('/contact', (req, res) => {
+    res.render("contact", {heading: 'Welcome to my contact page!'});
 });
 
 app.listen(port, () => {console.log("Server is listening on port 3000.")});
