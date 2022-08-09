d76063d614b2dfdda0cbc6dc1e5b0c699ceaca01
Adam Amatavivadhana <adam.amata@yahoo.com>
2022-08-09 11:09:33 +0200

    added styles

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require("express");
 const app = express();
 const port = 3000; 
 
-app.set("views", __dirname + "/views"); 
-app.set("view engine", "hbs"); 
+app.set("views", __dirname + "/views"); //setting views directory to /views
+app.set("view engine", "hbs"); //setting view engine to hbs 
+app.use(express.static('public')); //all static files in public folder are accessible by express
 
-const users = ['Adam', 'Sofia', 'Jom', 'Brendan'];
+const users = ['user1', 'user2', 'user3', 'user4'];
 
 app.get('/', (req, res) => {
     res.render("index", {heading: 'Welcome to my homepage!', users: users}); 
